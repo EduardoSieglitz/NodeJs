b98b64a9b88894fa43cf56b6452842d293c1bb1b
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-05-05 22:01:40 -0300

    Comentário
    Cadastro pergunta no nodejs finalizado.

--- CadastrarPergunta/index.js
+++ CadastrarPergunta/index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const app = express();
 const conexao = require("./database/dados");
 const Pergunta = require("./database/ControleCadastro.js")
+const bodys = require("body-parser");
+
 conexao.authenticate().then(() =>{
     console.log("Conexão foi bem sucedida")
 }).catch((msgErro)=>{
     console.log(msgErro)
 });
+
+app.use(bodys.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"))
 
@@ -17,6 +21,18 @@ app.get("/",(req, res)=>{
 app.get("/perguntar",(req, res)=>{
     res.render("perguntas/perguntar");
 });
+
+app.post("/salvarpergunta", (req, res)=>{
+var titulo = req.body.titulo;
+var descricao = req.body.descricao;
+Pergunta.create({
+    titulo:titulo,
+    descricao:descricao
+}).then(()=>{
+    res.redirect("/");
+})
+    
+})
 app.listen(3000, () => {
     console.log("Tudo ok");
 });
