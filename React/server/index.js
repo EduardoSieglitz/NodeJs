b1659e2ee7a050bc6aaa06d44a388e5618f0ff01
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-07-12 22:39:14 -0300

    Login
    Terminado a pagina de login com React.

--- React/server/index.js
+++ React/server/index.js
@@ -1,26 +1,6 @@
 const express = require("express"),
-    app = express(),
-    bd = require("./database/basedados"),
-    cors = require("cors");
-
-app.use(cors());
-app.use(express.json());
-
-app.post("/register", (req, res) => {
-    const { name } = req.body,
-        { email } = req.body,
-        { senha } = req.body,
-        { dia } = req.body,
-        { mes } = req.body,
-        { ano } = req.body,
-        datanascimento  = ano + "-" + mes + "-" + dia;
-    let SQL = "INSERT INTO tb_cliente (nome, email, senha, datanascimento) VALUES (?,?,?,?)";
-
-    bd.query(SQL, [name, email, senha, datanascimento], (err, result) => {
-        console.log(err);
-    })
-})
+app = express();
 
 app.listen(3001, () => {
-    console.log("Servidor rodando...");
-});
+    console.log("Servidor rodando...")
+})
