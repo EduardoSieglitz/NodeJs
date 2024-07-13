93121568a3af4e622e3069b92052ddc8cb9153b3
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-07-13 00:42:54 -0300

    Atualizando
    Arrumando erros

--- ReactExpress/server/database/basedados.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2");
-
-const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "cliente",
-});
-module.exports = db;
--- ReactExpress/server/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express"),
-app = express();
-
-app.listen(3001, () => {
-    console.log("Servidor rodando...")
-})
